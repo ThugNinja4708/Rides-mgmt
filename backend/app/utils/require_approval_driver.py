49b3e76ead37a0073981f6933f8722c19ac2aadf
Sahithi Pandiri <sahithi.pandiri@cyberark.com>
2024-12-11 12:00:10 +0530

    booking table pickup location

--- backend/app/utils/require_approval_driver.py
+++ backend/app/utils/require_approval_driver.py
@@ -1,12 +1,12 @@
 from app.models.Driver import Driver
 from app.utils.response import Response
-from flask import request
+from flask_jwt_extended import get_jwt_identity
 from functools import wraps
 def require_approval(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        driver_id = request.json.get("driver_id")
-        driver = Driver.get_by_id(driver_id)
+        driver_id = get_jwt_identity()
+        driver = Driver.get_by_id(driver_id).to_dict()
         if not driver or driver.get("status") != "approved":
             return Response.generate(status=403, message="Driver is not approved")
         return func(*args, **kwargs)
